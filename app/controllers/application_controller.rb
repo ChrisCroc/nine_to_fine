bce6d91f0d91088dda199ad30260f7a0d78b81f7
Christophe Crokaert <croc1014@me.com>
2026-05-26 17:58:06 +0200

    feat: set up devise with username, navbar, flashes and ath protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   stale_when_importmap_changes
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, unless: :devise_controller?
 
   protected
 
