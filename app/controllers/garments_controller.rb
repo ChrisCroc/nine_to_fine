fa1822795b79e88e0e9b1dfdd28432302cf63762
Christophe Crokaert <croc1014@me.com>
2026-05-29 16:09:58 +0200

    feat: add the routing + CRUD + form_with to garments

---
+++ app/controllers/garments_controller.rb
@@ -0,0 +1,55 @@
+class GarmentsController < ApplicationController
+
+  before_action :set_garment, only: %i[show edit update destroy]
+  before_action :set_categories, only: %i[new edit create update]
+  def index
+    @garments = current_user.garments.includes(:category).order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @garment = current_user.garments.new
+  end
+
+  def create
+    @garment = current_user.garments.new(garment_params)
+
+    if @garment.save
+      redirect_to @garment, notice: "Garment was successfully created."
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @garment.update(garment_params)
+      redirect_to @garment, status: :see_other, notice: "Garment was successfully updated."
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @garment.destroy
+    redirect_to garments_path, status: :see_other, notice: "Garment was successfully destroyed."
+  end
+
+private
+
+  def set_garment
+    @garment = current_user.garments.find(params[:id])
+  end
+
+  def garment_params
+    params.expect(garment: [:name, :color, :description, :brand, :category_id])
+  end
+
+  def set_categories
+    @categories = Category.order(:position)
+  end
+end
