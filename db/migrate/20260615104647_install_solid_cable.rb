a589298c025fc8be181e1a74c84aca6bbc2cfbff
Christophe Crokaert <croc1014@me.com>
2026-06-15 15:48:53 +0200

    chore(infra): rubocop autofix on Solid* migrations

--- db/migrate/20260615104647_install_solid_cable.rb
+++ db/migrate/20260615104647_install_solid_cable.rb
@@ -6,8 +6,8 @@ class InstallSolidCable < ActiveRecord::Migration[8.1]
       t.datetime "created_at", null: false
       t.integer "channel_hash", limit: 8, null: false
       t.index [ "channel" ], name: "index_solid_cable_messages_on_channel"
-      t.index [ "channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
-      t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+      t.index [ "channel_hash" ], name: "index_solid_cable_messages_on_channel_hash"
+      t.index [ "created_at" ], name: "index_solid_cable_messages_on_created_at"
     end
   end
 end
