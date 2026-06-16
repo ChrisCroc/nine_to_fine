a1d2cccc46716bed8241d0f8ee3fee3b3df86e5f
Christophe Crokaert <croc1014@me.com>
2026-06-16 15:01:25 +0200

    feat: broadcast comments in real time via Turbo Streams

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,20 @@ class Comment < ApplicationRecord
   belongs_to :outfit, counter_cache: :comments_count
 
   validates :body, presence: true, length: { maximum: 1000 }
+
+  after_create_commit -> {
+    broadcast_prepend_to outfit,
+    target: ActionView::RecordIdentifier.dom_id(outfit, :comments),
+    partial: "comments/comment", locals: { comment: self }
+  broadcast_replace_to outfit,
+    target: ActionView::RecordIdentifier.dom_id(outfit, :comments_count),
+    partial: "comments/comments_count", locals: { outfit: outfit }
+  }
+
+  after_destroy_commit -> {
+    broadcast_remove_to outfit
+    broadcast_replace_to outfit,
+      target: ActionView::RecordIdentifier.dom_id(outfit, :comments_count),
+      partial: "comments/comments_count", locals: { outfit: outfit }
+  }
 end
