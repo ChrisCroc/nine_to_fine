ef6d8f0e8dcb60e49e71457e697a6d549618fc83
Christophe Crokaert <croc1014@me.com>
2026-06-18 15:24:28 +0200

    Feat/profile edit (#127)
    * feat: edit own profile username via UsersController#edit/update
    
    * fix: correct cosmetic typos (follow button label/classes, outfit comments comment)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  before_action :require_self, only: %i[edit update]
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Profile updated"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_self
+    redirect_to user_path(@user), alert: "You can only edit your own profile." unless @user == current_user
+  end
+
+  def user_params
+    params.expect(user: [ :username ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :likes, only: %i[create destroy]
     resources :comments, only: %i[create destroy]
   end
-  resources :users, only: %i[show] do
+  resources :users, only: %i[show edit update] do
     resource :follow, only: %i[create destroy]
   end
   root to: "pages#home"
