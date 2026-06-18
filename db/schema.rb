dccee0af6bffa79a4a20208089eea24bf8767263
Christophe Crokaert <croc1014@me.com>
2026-06-18 10:31:35 +0200

    feat(follow): migration follows self-join + counter_cache columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_16_092852) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_18_082405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -58,6 +58,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_16_092852) do
     t.bigint "user_id", null: false
     t.index ["outfit_id"], name: "index_comments_on_outfit_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "followed_id", null: false
+    t.bigint "follower_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "garments", force: :cascade do |t|
@@ -272,6 +281,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_16_092852) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.integer "followers_count", default: 0, null: false
+    t.integer "following_count", default: 0, null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
@@ -285,6 +296,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_16_092852) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "outfits"
   add_foreign_key "comments", "users"
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "garments", "categories"
   add_foreign_key "garments", "users"
   add_foreign_key "likes", "users"
