e55246bfadd3318d3a61924dbf0c915252c1f32c
Christophe Crokaert <croc1014@me.com>
2026-06-18 11:11:48 +0200

    feat(follow): Follow model self-join + User associations + specs

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,13 @@
 class Follow < ApplicationRecord
-  belongs_to :follower
-  belongs_to :followed
+  belongs_to :follower, class_name: "User", counter_cache: :following_count
+  belongs_to :followed, class_name: "User", counter_cache: :followers_count
+
+  validates :follower_id, uniqueness: { scope: :followed_id }
+  validate :not_self_follow
+
+  private
+
+  def not_self_follow
+    errors.add(:followed_id, "can't follow yourself") if follower == followed
+  end
 end
