2af239c769eac93fe59fc7a417c38443cbfd7650
Christophe Crokaert <croc1014@me.com>
2026-07-08 01:14:04 +0200

    feat(home): three feature blocks grid

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,13 +3,22 @@ require "rails_helper"
 RSpec.describe "Pages", type: :request do
   describe "GET /" do
     context "when a user is not signed in" do
+      before { get root_path }
+
       it "returns a successful response (home is public)" do
-        get root_path
         expect(response).to have_http_status(:success)
+      end
+
+      it "renders a real text <h1> for SEO" do
+        expect(response.body).to include("<h1")
+      end
+
+      it "renders the three blocks as <h2> headings" do
+        expect(response.body.scan("<h2").size).to be >=3
       end
     end
 
-    context "when the user is signed in"
+    context "when the user is signed in" do
       let(:user) { create(:user) }
       before { sign_in user }
 
@@ -17,5 +26,6 @@ RSpec.describe "Pages", type: :request do
         get root_path
         expect(response).to have_http_status(:success)
       end
+    end
   end
 end
