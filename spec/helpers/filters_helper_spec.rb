07f752f425357e0e34a7642d9ddff5f747d17621
Christophe Crokaert <croc1014@me.com>
2026-07-13 12:32:37 +0200

    style: remove trailing whitespace in filters helper spec

--- spec/helpers/filters_helper_spec.rb
+++ spec/helpers/filters_helper_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe FiltersHelper, type: :helper do
   describe "#outfits_filter_count" do
     it "counts non-blank tag_ids plus one when a piece is set" do
       params = { tag_ids: [ "1", "2", "" ], garment_id: "7" }
-      
+
       expect(helper.outfits_filter_count(params)).to eq(3)
     end
   end
